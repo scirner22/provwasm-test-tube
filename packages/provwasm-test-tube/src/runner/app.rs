160e961b4fe58dda4f766f2fcb5baacdaa753697
Steve Cirner <scirner@provlabs.io>
2025-10-23 20:56:00 -0400

    fix

--- packages/provwasm-test-tube/src/runner/app.rs
+++ packages/provwasm-test-tube/src/runner/app.rs
@@ -7,7 +7,7 @@ use test_tube_prov::BaseApp;
 const FEE_DENOM: &str = "nhash";
 const PROV_ADDRESS_PREFIX: &str = "tp";
 const CHAIN_ID: &str = "testchain";
-const DEFAULT_GAS_ADJUSTMENT: f64 = 1.85;
+const DEFAULT_GAS_ADJUSTMENT: f64 = 1.00;
 
 #[derive(Debug, PartialEq)]
 pub struct ProvwasmTestApp {
